2322987c6357d32b10b2fb7f5a53ffbd3e1adb44
AKASH M <87299311+gamkers@users.noreply.github.com>
2025-02-08 09:54:29 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Replace <repository-name> with your GitHub repository name
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'build', // Output to 'build' directory
   },
 });
